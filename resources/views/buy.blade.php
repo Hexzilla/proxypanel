42a42e92af0bc54888399901cd30f1dc8e810198
rightvalue <bluebird6922@gamil.com>
2021-03-11 04:17:06 +0900

    update

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -62,35 +62,177 @@ label {
 <!-- End Page Header -->
 @endsection
 @section('content')
-<div class="bg-pink-transparent nav-bg">
-	<nav class="nav nav-tabs">
-		<a class="nav-link active" data-toggle="tab" href="#tabCont1">
-			<p class="text-center tab-title">DEDICATED 4G</p>
-			<p class="tab-paragraph"> </p>
-			<p class="tab-paragraph">- A large number of locations</p>
-			<p class="tab-paragraph">- Unlimited Bandwidth</p>
-		</a>
-		<a class="nav-link" data-toggle="tab" href="#tabCont2">
-			<p class="text-center tab-title">ROTATING</p>
-			<p class="tab-paragraph">- Access to huge pool of 4g modems</p>
-			<p class="tab-paragraph">- Modems change IP every 5 mins</p>
-			<p class="tab-paragraph">- Integrated with Multilogin</p>
-		</a>
-		<a class="nav-link" data-toggle="tab" href="#tabCont3">
-			<p class="text-center tab-title">MULTILOCATION</p>
-			<p class="tab-paragraph">- Access to huge pool of 4g modems</p>
-			<p class="tab-paragraph">- Set city at any moment</p>
-			<p class="tab-paragraph">- Integrated with Multilogin</p>
-		</a>
-	</nav>
-</div>
+<div>
+	<div class="bg-pink-transparent nav-bg">
+		<nav class="nav nav-tabs">
+			<a class="nav-link active" data-toggle="tab" href="#tabCont1">
+				<p class="text-center tab-title">DEDICATED 4G</p>
+				<p class="tab-paragraph"> </p>
+				<p class="tab-paragraph">- A large number of locations</p>
+				<p class="tab-paragraph">- Unlimited Bandwidth</p>
+			</a>
+			<a class="nav-link" data-toggle="tab" href="#tabCont2">
+				<p class="text-center tab-title">ROTATING</p>
+				<p class="tab-paragraph">- Access to huge pool of 4g modems</p>
+				<p class="tab-paragraph">- Modems change IP every 5 mins</p>
+				<p class="tab-paragraph">- Integrated with Multilogin</p>
+			</a>
+			<a class="nav-link" data-toggle="tab" href="#tabCont3">
+				<p class="text-center tab-title">MULTILOCATION</p>
+				<p class="tab-paragraph">- Access to huge pool of 4g modems</p>
+				<p class="tab-paragraph">- Set city at any moment</p>
+				<p class="tab-paragraph">- Integrated with Multilogin</p>
+			</a>
+		</nav>
+	</div>
 
-<div class="tab-pane active show" id="tabCont1">
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="card custom-card">
-				<div class="card-body">
-					@foreach($locations as $m)
+	<div class="tab-pane active show" id="tabCont1">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="card custom-card">
+					<div class="card-body">
+						@foreach($locations as $m)
+							<div class="row">    
+								<div class="col-md-3 col-lg-3 col-sm-3">
+									<label>
+										<input type="radio" name="product" class="card-input-element" />
+										<div class="panel panel-default card-input">
+										<div class="panel-heading text-center tx-16 mb-2">MONTHLY</div>
+										<div class="panel-body">
+											<p class="text-center tx-16 mb-1">{{$m}}</p>
+											<p class="text-center tx-14 mb-1">$175</p>
+										</div>
+										</div>
+									</label>
+								</div>
+								<div class="col-md-3 col-lg-3 col-sm-3">
+									<label>
+										<input type="radio" name="product" class="card-input-element" />
+										<div class="panel panel-default card-input">
+										<div class="panel-heading text-center tx-16 mb-2">WEEKLY</div>
+										<div class="panel-body">
+											<p class="text-center tx-16 mb-1">{{$m}}</p>
+											<p class="text-center tx-14 mb-1">$75</p>
+										</div>
+										</div>
+									</label>
+								</div>
+								<div class="col-md-3 col-lg-3 col-sm-6">
+									<label>
+										<input type="radio" name="product" class="card-input-element" />
+										<div class="panel panel-default card-input">
+										<div class="panel-heading text-center tx-16 mb-2">DAILY</div>
+										<div class="panel-body">
+											<p class="text-center tx-16 mb-1">{{$m}}</p>
+											<p class="text-center tx-14 mb-1">$20</p>
+										</div>
+										</div>
+									</label>
+								</div>
+								<div class="col-md-3 col-lg-3 col-sm-6">
+									<label>
+										<input type="radio" name="product" class="card-input-element" />
+										<div class="panel panel-default card-input">
+										<div class="panel-heading text-center tx-16 mb-2">ONE HOUR TRIAL</div>
+										<div class="panel-body">
+											<p class="text-center tx-16 mb-1">{{$m}}</p>
+											<p class="text-center tx-14 mb-1">$10</p>
+										</div>
+										</div>
+									</label>
+								</div>
+							</div>
+						@endforeach
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Row -->
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="card custom-card">
+					<div class="card-body">
+						<h3>Proxy Configurations</h3><br>
+						<div class="row">
+							<div class="col-lg-6 col-md-6 col-sm-6">
+								<p class="mg-b-5">Nickname: </p>
+								<input type="text" class="form-control" name="example-text-input" value="Proxy">
+								<p class="text-muted card-sub-title">A unique alphanumeric name for this proxy</p>
+							</div>
+							<div class="col-lg-6 col-md-6 col-sm-6">
+								<p class="mg-b-5">Rotation</p>
+								<select class="form-control select2-no-search">
+									<option label="No rotation"></option>
+									<option value="5">
+										5 Minutes
+									</option>
+									<option value="10">
+										10 Minutes
+									</option>
+									<option value="15">
+										15 Minutes
+									</option>
+									<option value="30">
+										30 Minutes
+									</option>
+									<option value="60">
+										1 Hour
+									</option>
+								</select>
+								<p class="text-muted card-sub-title">How frequent you want proxy to change, you can change this later</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Row -->
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="card custom-card">
+					<div class="card-body">
+						<h3>Payment Method</h3><br>
+						<div class="row payment-div">
+							<div class="col-md-6 col-lg-6 col-sm-6">
+								<label>
+									<input type="radio" name="payment" class="card-input-element" />
+									<div class="panel panel-default card-input">
+										<!-- <div class="panel-heading text-center tx-16 mb-2">PayPal</div> -->
+										<div class="panel-body">
+											<img src="{{asset('assets/img/pngs/paypal.png')}}" width="100%" height="100px"></img>
+										</div>
+									</div>
+								</label>
+							</div>
+							<div class="col-md-6 col-lg-6 col-sm-6">
+								<label>
+									<input type="radio" name="payment" class="card-input-element" />
+									<div class="panel panel-default card-input">
+										<!-- <div class="panel-heading text-center tx-16 mb-2">BTC</div> -->
+										<div class="panel-body">
+											<img src="{{asset('assets/img/pngs/bitcoin.png')}}" width="100%" height="100px" style="padding: 10px"></img>
+										</div>
+									</div>
+								</label>
+							</div>
+						</div>
+						<br>
+						<div class="text-right">
+							<button class="btn ripple btn-main-primary">Buy This Proxy</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="tab-pane" id="tabCont2">
+	</div>
+	<div class="tab-pane" id="tabCont3">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="card custom-card">
+					<div class="card-body">
 						<div class="row">    
 							<div class="col-md-3 col-lg-3 col-sm-3">
 								<label>
@@ -98,7 +240,6 @@ label {
 									<div class="panel panel-default card-input">
 									<div class="panel-heading text-center tx-16 mb-2">MONTHLY</div>
 									<div class="panel-body">
-										<p class="text-center tx-16 mb-1">{{$m}}</p>
 										<p class="text-center tx-14 mb-1">$175</p>
 									</div>
 									</div>
@@ -110,7 +251,6 @@ label {
 									<div class="panel panel-default card-input">
 									<div class="panel-heading text-center tx-16 mb-2">WEEKLY</div>
 									<div class="panel-body">
-										<p class="text-center tx-16 mb-1">{{$m}}</p>
 										<p class="text-center tx-14 mb-1">$75</p>
 									</div>
 									</div>
@@ -122,7 +262,6 @@ label {
 									<div class="panel panel-default card-input">
 									<div class="panel-heading text-center tx-16 mb-2">DAILY</div>
 									<div class="panel-body">
-										<p class="text-center tx-16 mb-1">{{$m}}</p>
 										<p class="text-center tx-14 mb-1">$20</p>
 									</div>
 									</div>
@@ -134,151 +273,11 @@ label {
 									<div class="panel panel-default card-input">
 									<div class="panel-heading text-center tx-16 mb-2">ONE HOUR TRIAL</div>
 									<div class="panel-body">
-										<p class="text-center tx-16 mb-1">{{$m}}</p>
 										<p class="text-center tx-14 mb-1">$10</p>
 									</div>
 									</div>
 								</label>
 							</div>
-						</div>
-					@endforeach
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- Row -->
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="card custom-card">
-				<div class="card-body">
-					<h3>Proxy Configurations</h3><br>
-					<div class="row">
-						<div class="col-lg-6 col-md-6 col-sm-6">
-							<p class="mg-b-5">Nickname: </p>
-							<input type="text" class="form-control" name="example-text-input" value="Proxy">
-							<p class="text-muted card-sub-title">A unique alphanumeric name for this proxy</p>
-						</div>
-						<div class="col-lg-6 col-md-6 col-sm-6">
-							<p class="mg-b-5">Rotation</p>
-							<select class="form-control select2-no-search">
-								<option label="No rotation"></option>
-								<option value="5">
-									5 Minutes
-								</option>
-								<option value="10">
-									10 Minutes
-								</option>
-								<option value="15">
-									15 Minutes
-								</option>
-								<option value="30">
-									30 Minutes
-								</option>
-								<option value="60">
-									1 Hour
-								</option>
-							</select>
-							<p class="text-muted card-sub-title">How frequent you want proxy to change, you can change this later</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<!-- Row -->
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="card custom-card">
-				<div class="card-body">
-					<h3>Payment Method</h3><br>
-					<div class="row payment-div">
-						<div class="col-md-6 col-lg-6 col-sm-6">
-							<label>
-								<input type="radio" name="payment" class="card-input-element" />
-								<div class="panel panel-default card-input">
-									<!-- <div class="panel-heading text-center tx-16 mb-2">PayPal</div> -->
-									<div class="panel-body">
-										<img src="{{asset('assets/img/pngs/paypal.png')}}" width="100%" height="100px"></img>
-									</div>
-								</div>
-							</label>
-						</div>
-						<div class="col-md-6 col-lg-6 col-sm-6">
-							<label>
-								<input type="radio" name="payment" class="card-input-element" />
-								<div class="panel panel-default card-input">
-									<!-- <div class="panel-heading text-center tx-16 mb-2">BTC</div> -->
-									<div class="panel-body">
-										<img src="{{asset('assets/img/pngs/bitcoin.png')}}" width="100%" height="100px" style="padding: 10px"></img>
-									</div>
-								</div>
-							</label>
-						</div>
-					</div>
-					<br>
-					<div class="text-right">
-						<button class="btn ripple btn-main-primary">Buy This Proxy</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
-<div class="tab-pane" id="tabCont2">
-	
-</div>
-
-<div class="tab-pane" id="tabCont3">
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="card custom-card">
-				<div class="card-body">
-					<div class="row">    
-						<div class="col-md-3 col-lg-3 col-sm-3">
-							<label>
-								<input type="radio" name="product" class="card-input-element" />
-								<div class="panel panel-default card-input">
-								<div class="panel-heading text-center tx-16 mb-2">MONTHLY</div>
-								<div class="panel-body">
-									<p class="text-center tx-14 mb-1">$175</p>
-								</div>
-								</div>
-							</label>
-						</div>
-						<div class="col-md-3 col-lg-3 col-sm-3">
-							<label>
-								<input type="radio" name="product" class="card-input-element" />
-								<div class="panel panel-default card-input">
-								<div class="panel-heading text-center tx-16 mb-2">WEEKLY</div>
-								<div class="panel-body">
-									<p class="text-center tx-14 mb-1">$75</p>
-								</div>
-								</div>
-							</label>
-						</div>
-						<div class="col-md-3 col-lg-3 col-sm-6">
-							<label>
-								<input type="radio" name="product" class="card-input-element" />
-								<div class="panel panel-default card-input">
-								<div class="panel-heading text-center tx-16 mb-2">DAILY</div>
-								<div class="panel-body">
-									<p class="text-center tx-14 mb-1">$20</p>
-								</div>
-								</div>
-							</label>
-						</div>
-						<div class="col-md-3 col-lg-3 col-sm-6">
-							<label>
-								<input type="radio" name="product" class="card-input-element" />
-								<div class="panel panel-default card-input">
-								<div class="panel-heading text-center tx-16 mb-2">ONE HOUR TRIAL</div>
-								<div class="panel-body">
-									<p class="text-center tx-14 mb-1">$10</p>
-								</div>
-								</div>
-							</label>
 						</div>
 					</div>
 				</div>
