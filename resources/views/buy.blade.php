f1682790feda1e67978cd215dc074248ee657fb3
rightvalue <bluebird6922@gamil.com>
2021-03-11 04:51:02 +0900

    update

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -33,8 +33,8 @@ label {
 	max-width: 700px;
 	margin: 0 auto;
 }
-.nav-item {
-	margin: 0;
+.nav-btn {
+	margin: 0 !important;
 }
 #tabCont1, #tabCont2, #tabCont3 {
 	margin-top: 20px;
@@ -68,19 +68,19 @@ label {
 <div>
 	<div class="nav-bg">
 		<nav class="nav nav-tabs">
-			<a class="nav-link active col-md-4 col-lg-4 nav-item" data-toggle="tab" href="#tabCont1">
+			<a class="nav-link active col-md-4 col-lg-4 nav-btn" data-toggle="tab" href="#tabCont1">
 				<p class="text-center tab-title">DEDICATED 4G</p>
 				<p class="tab-paragraph"> </p>
 				<p class="tab-paragraph">- A large number of locations</p>
 				<p class="tab-paragraph">- Unlimited Bandwidth</p>
 			</a>
-			<a class="nav-link col-md-4 col-lg-4 nav-item" data-toggle="tab" href="#tabCont2">
+			<a class="nav-link col-md-4 col-lg-4 nav-btn" data-toggle="tab" href="#tabCont2">
 				<p class="text-center tab-title">ROTATING</p>
 				<p class="tab-paragraph">- Access to huge pool of 4g modems</p>
 				<p class="tab-paragraph">- Modems change IP every 5 mins</p>
 				<p class="tab-paragraph">- Integrated with Multilogin</p>
 			</a>
-			<a class="nav-link col-md-4 col-lg-4 nav-item" data-toggle="tab" href="#tabCont3">
+			<a class="nav-link col-md-4 col-lg-4 nav-btn" data-toggle="tab" href="#tabCont3">
 				<p class="text-center tab-title">MULTILOCATION</p>
 				<p class="tab-paragraph">- Access to huge pool of 4g modems</p>
 				<p class="tab-paragraph">- Set city at any moment</p>
