26d9217f6c757df724336e658d403abfae22256a
rightvalue <bluebird6922@gamil.com>
2021-03-16 03:46:24 +0900

    update

---
+++ app/Http/Controllers/IpController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Port;
+
+class IpController extends Controller
+{
+    function changeIp(Request $request) {
+        // $res = Http::get('http://phones.proxypanel.io:1080/change?u='.$request->userInfo);
+        // var_dump($request->userInfo.' / '.$request->portId);
+        
+        $port = Port::find($request->portId);
+        $port->lastchangeipdate = date('Y-m-d H:i:s');
+        $port->save();
+
+        return redirect()->back();
+    }
+}
