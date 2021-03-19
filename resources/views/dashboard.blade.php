8eb1b6ab71254e44e706a5b485aee87eb49cb74a
rightvalue <bluebird6922@gamil.com>
2021-03-19 14:58:02 +0900

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -205,6 +205,26 @@
 	</div>
 </div>
 <!-- End Modal effects-->
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    
+	<input type="hidden" name="cmd" value="_xclick" id="id_cmd">
+	<input type="hidden" name="charset" value="utf-8" id="id_charset">
+	<input type="hidden" name="currency_code" value="USD" id="id_currency_code">
+	<input type="hidden" name="no_shipping" value="1" id="id_no_shipping">
+	<input type="hidden" name="business" value="vpnrgr@gmail.com" id="id_business">
+	<input type="hidden" name="amount" value="175.00" id="id_amount">
+	<input type="hidden" name="item_name" value="0.01$" id="id_item_name">
+	<input type="hidden" name="invoice" value="invoice-1-1534-1079-9" id="id_invoice">
+	<!-- <input type="hidden" name="notify_url" value="https://proxypanel.io/paypal/" id="id_notify_url"> -->
+	<input type="hidden" name="rm" value="1" id="id_rm">
+	<input type="hidden" name="return" value="{{route('/')}}" id="id_return">
+	<input type="hidden" name="cancel_return" value="{{route('/')}}" id="id_cancel_return">
+	<input type="hidden" name="custom" value="premium_plan" id="id_custom">
+	<div class="form-group">
+		<input class="btn btn-primary" type="submit" value="Test pay 0.01$">
+	</div>
+               
+</form>
 
 <!-- Modal effects -->
 <div class="modal" id="modaldemo9">
