01cdb8d34649be12faf7de5303a82770bdd946af
rightvalue <bluebird6922@gamil.com>
2021-03-19 14:11:27 +0900

    update

--- config/paypal.php
+++ config/paypal.php
@@ -12,14 +12,14 @@
     //         // 'service.EndPoint' => 'https://api-m.paypal.com/v2/checkout/orders/', 
     //     ),
     // ];
-    return [ 
-        'client_id' => env('PAYPAL_CLIENT_ID','Adc2E4SMJCpuoUYuyA_u7Ah-r-I0cNjF4kOOxiGd9jWZ9W4XOxt_iwLFHhTVvfLqXcx5gDAIY_hlSn9A'),
-        'secret' => env('PAYPAL_SECRET','EEdqWKA9WBmQ73pF-JhkP3Ux27tAKJq0Vym8jzYvvA0a11Gp3VGsAV1At--mPrLl8T3w5NlUX7Q9ZZMD'),
-        'settings' => array(
-            'mode' => env('PAYPAL_MODE','sandbox'),
-            'http.ConnectionTimeOut' => 30,
-            'log.LogEnabled' => true,
-            'log.FileName' => storage_path() . '/logs/paypal.log',
-            'log.LogLevel' => 'INFO'
-        ),
-    ];
+    // return [ 
+    //     'client_id' => env('PAYPAL_CLIENT_ID','Adc2E4SMJCpuoUYuyA_u7Ah-r-I0cNjF4kOOxiGd9jWZ9W4XOxt_iwLFHhTVvfLqXcx5gDAIY_hlSn9A'),
+    //     'secret' => env('PAYPAL_SECRET','EEdqWKA9WBmQ73pF-JhkP3Ux27tAKJq0Vym8jzYvvA0a11Gp3VGsAV1At--mPrLl8T3w5NlUX7Q9ZZMD'),
+    //     'settings' => array(
+    //         'mode' => env('PAYPAL_MODE','sandbox'),
+    //         'http.ConnectionTimeOut' => 30,
+    //         'log.LogEnabled' => true,
+    //         'log.FileName' => storage_path() . '/logs/paypal.log',
+    //         'log.LogLevel' => 'INFO'
+    //     ),
+    // ];
