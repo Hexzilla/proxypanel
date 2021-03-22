0cc297a3b226d96f7e1d8bf211dffd925deea7c7
rightvalue <bluebird6922@gamil.com>
2021-03-22 15:00:25 +0900

    update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -190,7 +190,7 @@ class PaymentController extends Controller
 
     public function notify(Request $request) {
         $pay = new Payments;
-        $pay->info = $request->item_name;
+        $pay->item_name = $request->item_name;
         $pay->date = date('Y-m-d H:i:s');
         $pay->currency = $request->mc_currency;
         $pay->payer_email = $request->payer_email;
